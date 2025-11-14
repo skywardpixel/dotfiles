d7d5e597b5e59b6de9b4defaec3ff04100360d03
Kyle Yan <kyleyan@google.com>
2025-11-13 23:30:29 -0800

    Import from internal repo

--- private_dot_config/nvim/lua/plugins/ui.lua
+++ private_dot_config/nvim/lua/plugins/ui.lua
@@ -114,9 +114,9 @@ return {
       -- LSP
       { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
       { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
-      { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
-      { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
-      { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+      { "grr", function() Snacks.picker.lsp_references() end, desc = "References" },
+      { "gri", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
+      { "grt", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto Type Definition" },
       { "gai", function() Snacks.picker.lsp_incoming_calls() end, desc = "C[a]lls Incoming" },
       { "gao", function() Snacks.picker.lsp_outgoing_calls() end, desc = "C[a]lls Outgoing" },
       { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
