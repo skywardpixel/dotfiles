7b8db41e420fd787d9ce27ca15f82820ceae22ac
Kyle Yan <kyleyan@google.com>
2025-11-26 16:16:48 -0800

    Import from internal repo

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -1638,8 +1638,8 @@
   ####################################[ time: current time ]####################################
   # Current time color.
   typeset -g POWERLEVEL9K_TIME_FOREGROUND=66
-  # Format for the current time: 09:51:02. See `man 3 strftime`.
-  typeset -g POWERLEVEL9K_TIME_FORMAT='%D{%I:%M:%S %p}'
+  # Format for the current time: 09:51 AM. See `man 3 strftime`.
+  typeset -g POWERLEVEL9K_TIME_FORMAT='%D{%I:%M %p}'
   # If set to true, time will update when you hit enter. This way prompts for the past
   # commands will contain the start times of their commands as opposed to the default
   # behavior where they contain the end times of their preceding commands.
